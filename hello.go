ded20c46069d8762dcc89a3f60eb47c953f8f9c3
anraku <anraku90@gmail.com>
2016-05-08 19:17:17 +0900

    use newmath package

--- hello.go
+++ hello.go
@@ -1,8 +1,13 @@
 package main 
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/anraku/newmath"
+	)
 
 func main(){
-	fmt.Printf("Hello, World!\n")
+	fmt.Printf("Hello, World. Sqrt(2) = %v\n", newmath.Sqrt(2)) 
 }
+
 
